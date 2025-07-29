04699ccb9ac5d0d606e265de11840f1a235e7ffa
Alessio230801 <alessioporri@gmail.com>
2025-07-29 16:45:11 +0200

    Update vite-env.d.ts speriamo bene

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@emailjs/browser']
+    }
+  }
+});
